27dd01c4dd8dcf696375439a579ed9197482aa44
anumitaverma <jhabloo1466@gmail.com>
2018-04-13 11:55:23 -0400

    Add files via upload

---
+++ About_Me.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+var container = document.getElementById('country_1');
+var windowh=window.innerHeight;
+var windoww=window.innerWidth;
+var scrollA= 1000-windowh;
+var image1=document.getElementsByName('image')[0];
+var image2=document.getElementsByName('image')[1];
